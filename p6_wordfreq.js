cd64f8fb2991a163f11543f1182edbc4c742d4cf
seongyeon min <mmminn@kw.ac.kr>
2026-09-11 15:15:16 +0900

    p6

--- p6_wordfreq.js
+++ p6_wordfreq.js
@@ -14,6 +14,7 @@
 
 import fs from "node:fs";
 // TODO: import { tokenize, countWords, topN } from ...
+import { tokenize, countWords, topN } from "./utils.js";
 
 const path = process.argv[2];
 const text = fs.readFileSync(path, "utf8");
@@ -23,6 +24,10 @@ const counts = countWords(words);
 
 // TODO: print "<total> words, <distinct> distinct"
 //       Object.keys(counts).length is the number of distinct words.
+console.log(`${words.length} words, ${Object.keys(counts).length} distinct`);
 
 // TODO: print the top 5 as "word count", one per line
 //       topN(counts, 5) returns [["code", 9], ["the", 7], ...]
+topN(counts, 5).forEach(([word, count]) => {
+  console.log(`${word}     ${count}`);
+});
